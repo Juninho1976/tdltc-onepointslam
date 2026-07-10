f4397a96ccf84bf8a83e777fa97e3eaaf26f26d8
Rakesh Kotecha <rakoosh@hotmail.com>
2026-07-10 23:25:56 +0100

    Oscillating issue fix again plus branding

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 const CONFIG = {
   TOURNAMENT_NAME: "TDLTC One Point Slam",
+  RESULTS_SHEET_ID: "1-nR9fuFvVi1e2f2zEKe9frTDP_WpGFQaNejoTcjKQ1Y",
+  RESULTS_GID: "936921154",
   RESULTS_CSV_URL:
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vRxYJhtOr2ymePcOAcH07bEXTAAYbL_Hg6OjJtOeDbfHrC52ezo3RHXISzjBc5WBTUnQIS7WEOQQl4u/pub?gid=624876732&single=true&output=csv",
+    "https://docs.google.com/spreadsheets/d/1-nR9fuFvVi1e2f2zEKe9frTDP_WpGFQaNejoTcjKQ1Y/pub?gid=936921154&single=true&output=csv",
   REFRESH_INTERVAL_MS: 5000,
 };
